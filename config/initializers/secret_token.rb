b5294ecc38ec79b384772e68db7eb1c7ff5d29dd
marlenycotrim <marlenycotrim@gmail.com>
2014-03-01 12:14:57 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bettervetter::Application.config.secret_token = 'dd50b811f31464f4b98349918724bf9015b2dac1aff4e200269925f3868520c991ee303736de68c153489dd2abbf9138b462239160132e7bb1c3750d5a598571'
